73a76828171447849d577a0d0b12273f03b06803
beth98an <bke.evans@gmail.com>
2022-05-18 16:48:26 +0100

    fully functional

--- app.py
+++ app.py
@@ -1,18 +1,28 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, request
 from flask_cors import CORS
 from controllers import urls
-
 
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('home.html')
+    if request.method == 'GET':
+        return render_template('home.html', link='', urlsearch='')
+    if request.method == 'POST':
+        data = request.form
+        link = urls.create(data)
+    return render_template('home.html', link=link, urlsearch=data['urlsearch'])
 
 @app.route('/<code>')
 def get_url(code):
     getUrl = urls.get(code)
-    return redirect(f"https://{getUrl}")
+    if getUrl:
+        return redirect(f"https://{getUrl}")
+    else: 
+        return redirect('/')
 
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- controllers/urls.py
+++ controllers/urls.py
@@ -1,4 +1,6 @@
 import sqlite3
+import random
+import string
 
 def getUrls(data):
     return {'id': data[0], 'url': data[1],'short_url': data[2]}
@@ -11,5 +13,31 @@ def get_db_connection():
 def get(code):
     conn = get_db_connection()
     findUrl = conn.execute('SELECT * FROM shorten_url WHERE short_url = ?', (code,)).fetchall()
-    getUrl = list(map(getUrls, findUrl))
-    return getUrl[0]['url']
+    if findUrl:
+        getUrl = list(map(getUrls, findUrl))
+        print(getUrl)
+        conn.close()
+        return getUrl[0]['url']
+    else:
+        return ''
+
+def random_string():
+    characters = string.ascii_lowercase + string.digits
+    str = ''.join(random.choice(characters) for i in range(6))
+    return str
+
+
+def create(data):
+    conn = get_db_connection()
+    check = conn.execute('SELECT * FROM shorten_url WHERE url = ?', (data['urlsearch'],)).fetchall()
+    if check :
+        getUrl = list(map(getUrls, check))
+        return getUrl[0]['short_url']
+    else:
+        getShort = random_string()
+        addUrl = conn.execute('INSERT INTO shorten_url (url, short_url) VALUES (?, ?) RETURNING *', (data['urlsearch'], getShort))
+        shortUrl = list(map(getUrls, addUrl))
+        print(shortUrl)
+        conn.commit()
+        conn.close()
+        return shortUrl[0]['short_url']
